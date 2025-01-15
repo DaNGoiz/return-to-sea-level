fd8caa3c89f5a3fd9e20f3abfbd096ba4eb3266f
DaNGo_iz <3332099165@qq.com>
2025-01-15 14:46:58 +0800

    导入项目

---
+++ CiGA2025Spring/Assets/Plugins/Messenger/MsgType.cs
@@ -0,0 +1,9 @@
+public static class MsgType
+{
+    public static string SimpleEvent = nameof(SimpleEvent);
+    public static string IntEvent = nameof(IntEvent);
+
+    public static string AddTextToLog = nameof(AddTextToLog);
+    public static string InterruptDialog = nameof(InterruptDialog);
+    public static string AutoPlay = nameof(AutoPlay);
+}
