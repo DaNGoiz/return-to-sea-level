b7ad29c12853956c025d7f33d4be32a698bdc078
UnderCloud <ssccww123456789@outlook.com>
2025-01-19 00:10:33 +0800

    气泡碰壁自动撞破，充气功能，玩家新增Attacker

---
+++ CiGA2025Spring/Assets/Scripts/Interface/PlayerAttacker.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacker : IAttacker
+{
+    [SerializeField]
+    private float dmg;
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        TryAttack(other, "Player", dmg);
+    }
+}
--- CiGA2025Spring/Assets/Scripts/RollingMap/UnderwaterObjects/LittleBubble.cs
+++ CiGA2025Spring/Assets/Scripts/RollingMap/UnderwaterObjects/LittleBubble.cs
@@ -11,9 +11,10 @@ public class LittleBubble : PickableObject
         Messenger.Broadcast(MsgType.ChangeBubbleBar, 1, 5f);
         DestroySelf();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    protected new void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.otherCollider.gameObject.layer == LayerMask.GetMask("UnderwaterObject"))
+        base.OnTriggerEnter2D(collision);
+        if (collision.gameObject.layer == 9)
         {
             BoomEffect.Set(transform.position, 0.4f);
             DestroySelf();
