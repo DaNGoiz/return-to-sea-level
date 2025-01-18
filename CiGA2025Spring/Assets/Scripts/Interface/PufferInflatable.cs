c0720e417b03b173a948219d6ae7161c9e99722e
UnderCloud <ssccww123456789@outlook.com>
2025-01-19 01:47:30 +0800

    修复气泡值恢复错误

--- CiGA2025Spring/Assets/Scripts/Interface/PufferInflatable.cs
+++ CiGA2025Spring/Assets/Scripts/Interface/PufferInflatable.cs
@@ -13,7 +13,30 @@ public class PufferInflatable : IInflatable
     public void Explode()
     {
         BoomEffect.Set(transform.position, 0.8f);
-        //发射n个气泡球
+        //发射1-4个气泡球
+        int bubbleNum = Random.Range(1, 4);
+        for (int i = 0; i < bubbleNum; i++)
+        {
+            StartCoroutine(BubbleSpread());
+        }
         Destroy(gameObject);
+
+        static IEnumerator BubbleSpread()
+        {
+            Vector3 dir;
+            float speed;
+            GameObject bubbleGo;
+            speed = Random.Range(0.03f, 0.1f);
+
+            bubbleGo = Instantiate(Resources.Load<GameObject>("Prefabs/Map/LittleBubble"));
+
+            dir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+            for (int i = 0; i <= 40; i++)
+            {
+                bubbleGo.transform.position += dir * speed;
+                speed /= 10f;
+                yield return null;
+            }
+        }
     }
 }
