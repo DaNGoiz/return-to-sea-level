f314d5738bcdd023ceb1223e308248e233d1dd97
UnderCloud <ssccww123456789@outlook.com>
2025-01-18 10:37:53 +0800

    改变玩家和地图的sortingLayer

--- CiGA2025Spring/Assets/Scripts/RollingMap/MapInitializer.cs
+++ CiGA2025Spring/Assets/Scripts/RollingMap/MapInitializer.cs
@@ -11,7 +11,7 @@ public static class MapInitializer
         //初始化地图管理器
         MapManager.Instance.Init();
         //添加地图管理器逻辑到相关事件的监听
-        //Messenger.AddListener(MsgType.GameStart, () => MapManager.Roll = true);
+        Messenger.AddListener(MsgType.GameStart, () => MapManager.Roll = true);
         Messenger.AddListener(MsgType.GameOver, () => MapManager.Roll = false);
         Messenger.AddListener(MsgType.GameRestart, MapManager.Instance.ResetMap);
     }
