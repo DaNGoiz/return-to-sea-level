3d25ced0193d3e636e280e3e92f3915e92bce936
UnderCloud <ssccww123456789@outlook.com>
2025-01-19 15:44:12 +0800

    河豚王

--- CiGA2025Spring/Assets/Scripts/Interface/PufferInflatable.cs
+++ CiGA2025Spring/Assets/Scripts/Interface/PufferInflatable.cs
@@ -14,7 +14,7 @@ public class PufferInflatable : IInflatable
     {
         BoomEffect.Set(transform.position, 0.8f * Mathf.Abs(transform.localScale.x));
         //·¢Éä1-4¸öÆøÅÝÇò
-        int bubbleNum = Random.Range(1, 4);
+        int bubbleNum = Random.Range(2, 5);
         for (int i = 0; i < bubbleNum; i++)
         {
             GameObject go = Instantiate(Resources.Load<GameObject>("Prefabs/Map/LittleBubble"));
