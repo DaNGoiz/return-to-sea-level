ef9507180caa3ad2f4e7d511d2a7f3d5bb0dcf1f
UnderCloud <ssccww123456789@outlook.com>
2025-01-19 14:21:36 +0800

    添加超级糖果

---
+++ CiGA2025Spring/Assets/Scripts/RollingMap/UnderwaterObjects/SuperSugar.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperSugar : PickableObject
+{
+    protected override void Pick(int playerNum)
+    {
+        base.Pick(playerNum);
+        BoomEffect.Set(transform.position, 1f * transform.localScale.x);
+        Messenger.Broadcast(MsgType.ChangeBubbleBar, playerNum, 30f * transform.localScale.x);
+        Messenger.Broadcast(MsgType.InfBubble, playerNum, Random.Range(3f, 7f));
+        DestroySelf();
+    }
+}
