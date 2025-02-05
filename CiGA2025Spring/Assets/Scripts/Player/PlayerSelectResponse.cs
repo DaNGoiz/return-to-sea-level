526c47c57c775e9f3bb42f7a490d2fc34c21dbc4
DaNGo_iz <3332099165@qq.com>
2025-02-05 13:30:42 +0800

    选择玩家按钮

--- CiGA2025Spring/Assets/Scripts/Player/PlayerSelectResponse.cs
+++ CiGA2025Spring/Assets/Scripts/Player/PlayerSelectResponse.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerSelectResponse : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    private GameObject player1component;
+    private GameObject player2component;
     void Start()
     {
+        // 在子物体里寻找带有player1和player2的tag的物体，绑定到component上
+
+        // Messenger listener
+        // Messenger.AddListener(MsgType.)
         
+        // 读取存在global data的目前玩家，动态加载玩家框
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Player1CompChangeState(bool isPlayer1Selected)
     {
-        
+        // 这个值等于playercomponent的set active
     }
+
+    private void Player2CompChangeState(bool isPlayer2Selected)
+    {
+        // 这个值等于playercomponent的set active
+    }
+
 }
