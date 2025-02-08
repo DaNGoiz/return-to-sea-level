74c9b4510d98cad3c0693897b829cd688dab1ee1
DaNGo_iz <3332099165@qq.com>
2025-02-08 15:43:38 +0800

    提示

---
+++ CiGA2025Spring/Assets/Scripts/UI/Tips.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tips : MonoBehaviour
+{
+    private Sprite sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("A");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
